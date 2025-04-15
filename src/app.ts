f6f82f3c1785a33e5dc4f7d3aa2c6da529ae41ec
unknown <vansurya.01@gmail.com>
2025-04-15 17:10:58 +0700

    feat: add feature login and register

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import indexRoutes from "./routes/index.routes";
 
 dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Travel Hunt API');
+app.use("/api", indexRoutes);
+app.get("/", (req, res) => {
+  res.send("Welcome to Travel Hunt API");
 });
 
 export default app;
