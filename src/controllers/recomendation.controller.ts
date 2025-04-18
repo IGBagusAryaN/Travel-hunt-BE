615cf9a7a70f67630e77d4580cbadf8baebc7765
unknown <vansurya.01@gmail.com>
2025-04-18 16:45:50 +0700

    chore: fix recomen

--- src/controllers/recomendation.controller.ts
+++ src/controllers/recomendation.controller.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from 'express';
 import { getDataFromAPI } from '../utils/apiData';
 
-
 function convertToFuzzyWeights(weights: Record<string, number>): Record<string, [number, number]> {
   const fuzzyWeights: Record<string, [number, number]> = {};
   for (const name in weights) {
     const value = weights[name];
-
-
     const lower = Math.max(0, value - 0.05); 
     const upper = Math.min(1, value + 0.05); 
     fuzzyWeights[name] = [lower, upper];
@@ -15,10 +12,9 @@ function convertToFuzzyWeights(weights: Record<string, number>): Record<string, 
   return fuzzyWeights;
 }
 
-
 function calculateFuzzyAHP(places: any[], weights: { [key: string]: [number, number] }): any[] {
   return places.map(place => {
-    const totalScore = place.place_scores.reduce((total : any, score: any) => {
+    const totalScore = place.place_scores.reduce((total: any, score: any) => {
       const weight = weights[score.criteriasId];
       if (!weight || !Array.isArray(weight)) {
         throw new Error(`Bobot tidak ditemukan atau format salah untuk kriteria: ${score.criteriasId}`);
@@ -35,7 +31,6 @@ function calculateFuzzyAHP(places: any[], weights: { [key: string]: [number, num
   }).sort((a, b) => b.totalScore - a.totalScore);
 }
 
-
 async function getRecommendations(req: Request, res: Response): Promise<Response> {
   const { city, weights } = req.body;
   
@@ -44,23 +39,17 @@ async function getRecommendations(req: Request, res: Response): Promise<Response
   }
 
   try {
- 
-    const places = await getDataFromAPI(city);
+    const cityData = await getDataFromAPI(city); // 👈 fix disini
+    const places = cityData.places; // 👈 ambil array tempatnya
 
-  
     const fuzzyWeights = convertToFuzzyWeights(weights);
-
     const result = calculateFuzzyAHP(places, fuzzyWeights);
 
-   
     return res.json({ recommendations: result });
   } catch (error: unknown) {
-
     if (error instanceof Error) {
-    
       return res.status(500).json({ error: error.message });
     } else {
-   
       return res.status(500).json({ error: 'An unknown error occurred' });
     }
   }
